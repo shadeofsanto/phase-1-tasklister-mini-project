3bbd2ab0d209e77d3b45a22e07d7000a5c08a13b
Joseph McCafferty <mcafferty.j11@gmail.com>
2023-05-21 01:28:40 -0500

    Task displayed success!

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+     addingEventListeners()
+})
+
+function addingEventListeners() {
+    document
+      .getElementById("create-task-form")
+      .addEventListener("submit", handleFormSubmit)
+}
+
+function handleFormSubmit(e) {
+  e.preventDefault()
+  // console.log(e)
+  const task = e.target["new-task-description"].value
+  console.log(task)
+
+  displayTask(task)
+}
+
+function displayTask(toDoTask) {
+  const taskUl = document.getElementById("tasks")
+  // console.log(taskUl)
+  const taskLi = document.createElement("li")
+  taskLi.textContent = toDoTask
+  // console.log(taskLi)
+  taskUl.appendChild(taskLi)
+}
